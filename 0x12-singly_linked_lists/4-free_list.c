3e6f2ffa0256901bf52417a56ec5796277f63d1f
THEOTHERGUY23 <mzimnguni23@gmail.com>
2023-10-19 15:04:16 -0700

    link

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* free_list - frees a list
+* @head: head of the linked list.
+* Return: nothing.
+*/
+
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
+}
